ba11f0c2d7e44238d2c6567b9fada4cb72c533a9
Cátia Teixeira <catia.rds.teixeira@gmail.com>
2023-02-26 22:40:35 +0000

    Fixed imports topic and added connection to board
    Signed-off-by: Cátia Teixeira <catia.rds.teixeira@gmail.com>

--- code/Game.py
+++ code/Game.py
@@ -1,36 +1,42 @@
 import pygame
 import sys
-import main
+import config
 
 
-class InitialScreen:
-    def __init__(self):
-        self.human_player = None
-        self.computer_player = None
+class Screen:
+    def __init__(self, WINDOW_SURF, main_clock, FONT):
         self.titles = {
-            'Which game mode do you want to chose?': ["Human vs Human", "Computer vs Human", "Computer vs Computer"],
-            'In which board size would you like to play with?': ["3 X 3", "5 X 5", "5 X 9"],
-            'What is your difficulty level?': ["Easy", "Medium", "Hard"],
-            'White always goes first. Do you want to be white or black?': ["White", "Black"],
+            'mode': {'Which game mode do you want to chose?': ["Human vs Human", "Computer vs Human",
+                                                               "Computer vs Computer"]},
+            'size': {'In which board size would you like to play with?': ["3 X 3", "5 X 5", "5 X 9"]},
+            'difficulty': {'What is your difficulty level?': ["Easy", "Medium", "Hard"]},
+            'token': {'White always goes first. Do you want to be white or black?': ["White", "Black"]},
         }
+        self.WINDOW_SURF = WINDOW_SURF
+        self.main_clock = main_clock
+        self.FONT = FONT
+        self.button_width = 110
+        self.button_height = 30
+        self.button_x = config.WINDOW_WIDTH - self.button_width - 10
+        self.button_y = config.WINDOW_HEIGHT - self.button_height - 10
         self.selected_options = {}
 
-    def draw_option(self, title, options, y):
+    def draw_option(self, key, title, options, y):
         # Choose game mode
-        text = main.BIG_FONT.render(title, True, main.BLACK)
+        text = self.FONT.render(title, True, config.BLACK)
         rect = text.get_rect()
-        rect.center = (int(main.WINDOW_WIDTH * 0.5), int(main.WINDOW_HEIGHT * y))
+        rect.center = (int(config.WINDOW_WIDTH * 0.5), int(config.WINDOW_HEIGHT * y))
 
         text_list = []
-        delta_x = 1./(len(options)+1)
+        delta_x = 1. / (len(options) + 1)
         x = delta_x
-        text_list.append((text, rect, title, False, False))
+        text_list.append((key, text, rect, title, False, False))
         for opt in options:
-            surf = main.BIG_FONT.render(opt, True, main.BLACK)
+            surf = self.FONT.render(opt, True, config.BLACK)
             rect = surf.get_rect()
-            rect.center = (int(main.WINDOW_WIDTH * x), int(main.WINDOW_HEIGHT * (y + 0.1)))
-            selected = self.selected_options.get(title, None) == opt
-            text_list.append((surf, rect, opt, selected, True))
+            rect.center = (int(config.WINDOW_WIDTH * x), int(config.WINDOW_HEIGHT * (y + 0.1)))
+            selected = self.selected_options.get(key, None) == opt
+            text_list.append((key, surf, rect, opt, selected, True))
             x += delta_x
 
         return text_list
@@ -40,10 +46,21 @@ class InitialScreen:
         delta_y = (1 / len(self.titles))
         text_list = []
         for key, value in self.titles.items():
-            text_list.append(self.draw_option(key, value, y))
-            y += delta_y
+            for title, options in value.items():
+                text_list.append(self.draw_option(key, title, options, y))
+                y += delta_y
 
         return text_list
+
+    def draw_readytostart(self):
+        # create the button surface and text
+        button_surface = pygame.Surface((self.button_width, self.button_height))
+        button_text = pygame.font.Font(None, 22).render("Ready to start", False, (255, 255, 255))
+
+        # fill the button surface with a color
+        button_surface.fill((0, 128, 0))
+
+        return button_surface, button_text
 
     def draw_initial_screen(self):
         """Draws the text and handles the mouse click events for letting
@@ -52,33 +69,91 @@ class InitialScreen:
         [BLACK_TOKEN, WHITE_TOKEN] if Black.
         """
         text_list = self.draw_all_options()
+        button_surface, button_text = self.draw_readytostart()
 
         while True:  # keep looping until the player has clicked on a color
-            main.main_clock.tick(main.FPS)  # CPU is too fast that slowing down is needed
-            main.WINDOW_SURF.fill(main.WHITE)
+
+            self.main_clock.tick(config.FPS)  # CPU is too fast that slowing down is needed
+            self.WINDOW_SURF.fill(config.WHITE)
+            # blit the button text onto the button surface
+            button_rect = button_text.get_rect(center=(self.button_width / 2, self.button_height / 2))
+            button_surface.blit(button_text, button_rect)
+
+            # blit the button surface onto the screen surface
+            self.WINDOW_SURF.blit(button_surface, (self.button_x, self.button_y))
+
             for opt in text_list:
-                for i, (surf, rect, text, selected, selectable) in enumerate(opt):
-                    color = main.GREEN if selected else main.BLACK # change text color based on selection status
+                for i, (key, surf, rect, text, selected, selectable) in enumerate(opt):
+                    color = config.GREEN if selected else config.BLACK  # change text color based on selection status
                     if selectable:
-                        new_surf = main.BIG_FONT.render(text, True, color)
-                        main.WINDOW_SURF.blit(new_surf, rect)
-                        opt[i] = (new_surf, rect, text, selected, selectable)  # update the text_list to reflect changes
+                        new_surf = self.FONT.render(text, True, color)
+                        self.WINDOW_SURF.blit(new_surf, rect)
+                        opt[i] = (
+                        key, new_surf, rect, text, selected, selectable)  # update the text_list to reflect changes
                     else:
-                        main.WINDOW_SURF.blit(surf, rect)
+                        self.WINDOW_SURF.blit(surf, rect)
             for event in pygame.event.get():  # event handling loop
-                main.main_clock.tick(main.FPS)
+                self.main_clock.tick(config.FPS)
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == main.MOUSEBUTTONDOWN and event.button == 1:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     # print 'click'
                     mouse_x, mouse_y = event.pos
                     for opt in text_list:
-                        for i, (surf, rect, text, selected, selectable) in enumerate(opt):
+                        for i, (key, surf, rect, text, selected, selectable) in enumerate(opt):
                             if rect.collidepoint((mouse_x, mouse_y)) and selectable:
-                                self.selected_options[opt[0][2]] = opt[i][2]  # update selected option
-                                for j, (s, r, t, sel, sela) in enumerate(opt):
-                                    opt[j] = (s, r, t, t == opt[i][2], True)  # update other options
-            main.main_clock.tick(main.FPS)
+                                self.selected_options[opt[0][0]] = opt[i][3]  # update selected option
+                                for j, (k, s, r, t, sel, sela) in enumerate(opt):
+                                    opt[j] = (k, s, r, t, t == opt[i][3], True)  # update other options
+
+                    button_clicked = button_rect.collidepoint(mouse_x - self.button_x, mouse_y - self.button_y)
+                    if button_clicked:
+                        pygame.event.clear()
+                        return self.selected_options
+
+            self.main_clock.tick(config.FPS)
             pygame.display.update()
-            main.main_clock.tick(main.FPS)
+            self.main_clock.tick(config.FPS)
+
+    def draw_grid(self, grid, board):
+        self.WINDOW_SURF.fill(config.WHITE)
+        # self.WINDOW_SURF.blit(
+        #     BG_IMAGE,
+        #     (int(config.WINDOW_WIDTH * 0.25), int(config.WINDOW_HEIGHT * 0.25)))
+
+        for column in range(board.GRID_COLS):
+            for row in range(board.GRID_ROWS):
+                center_pixel_coord = self.translate_grid_to_pixel_coord((column, row), board)
+                # draw token with outline
+                if grid[column][row]['token_color'] != config.EMPTY:
+                    pygame.draw.circle(
+                        self.WINDOW_SURF,
+                        grid[column][row]['token_color'],
+                        center_pixel_coord,
+                        int(board.GRID_SIZE * 0.5))
+
+                    pygame.draw.circle(
+                        self.WINDOW_SURF,
+                        config.BLACK,
+                        center_pixel_coord,
+                        int(board.GRID_SIZE * 0.5),
+                        2)
+
+        self.main_clock.tick(config.FPS)
+        pygame.display.update()
+        self.main_clock.tick(config.FPS)
+
+    def translate_grid_to_pixel_coord(self, coords, board):
+        (grid_column, grid_row) = coords
+        if board.GRID_COLS == 3:
+            x = grid_column * int(config.WINDOW_HEIGHT * 0.25) + int(config.WINDOW_HEIGHT * 0.25), \
+                grid_row * int(config.WINDOW_WIDTH * 0.25) + int(config.WINDOW_WIDTH * 0.25)
+        if board.GRID_COLS == 5:
+            x = grid_column * int(config.WINDOW_HEIGHT * 0.125) + int(config.WINDOW_HEIGHT * 0.25), \
+                grid_row * int(config.WINDOW_WIDTH * 0.125) + int(config.WINDOW_WIDTH * 0.25)
+
+        # if cols == 9:
+        # solve me
+
+        return x
--- code/main.py
+++ code/main.py
@@ -2,32 +2,41 @@ import pygame
 import sys
 from pygame.locals import *
 from copy import deepcopy
-import Board
-import Player
-from Game import InitialScreen
-
-# -------------------------------------------------- CONSTANTS --------------------------------------------------------#
-FPS = 30
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GRAY = (100, 100, 100)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-EMPTY = None
+from Board import Board
+#import Player
+from Game import Screen
+import config
 
 # Initialize pygame and the main clock
 pygame.init()
 main_clock = pygame.time.Clock()
 
 # Set up the window
-WINDOW_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+WINDOW_SURF = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
 pygame.display.set_caption('Fanorona')
-SMALL_FONT = pygame.font.Font(None, 8)
-BIG_FONT = pygame.font.Font(None, 12)
 
-screen = InitialScreen()
-screen.draw_initial_screen()
+# Set up the fonts
+SMALL_FONT = pygame.font.Font(None, 10)
+BIG_FONT = pygame.font.Font(None, 15)
+
+# Draw Initial Screen
+screen = Screen(WINDOW_SURF, main_clock, BIG_FONT)
+options = []
+while len(options) < 4:
+    options = screen.draw_initial_screen()
+    print(options)
+
+# Draw Board
+game_mode = options['mode']
+board_size = [int(n) for n in options['size'].split() if n != 'X']
+difficulty = options['difficulty']
+token = options['token']
+
+print(board_size)
+
+
+board = Board(board_size[0], board_size[1])
+grid = board.get_new_grid(board_size[0], board_size[1])
+
+while True:
+    screen.draw_grid(grid, board)

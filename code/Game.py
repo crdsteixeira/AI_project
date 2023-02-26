15e9745f769e77fc8876569105d24829873f8214
Cátia Teixeira <catia.rds.teixeira@gmail.com>
2023-02-26 19:10:11 +0000

    Added initial screen
    Changes to intial screen and main.
    
    Signed-off-by: Cátia Teixeira <catia.rds.teixeira@gmail.com>

--- code/Game.py
+++ code/Game.py
@@ -1,32 +1,49 @@
-from pygame.locals import *
-from main import *
-
-FPS = 30
-WINDOW_WIDTH = 640
-WINDOW_HEIGHT = 480
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GRAY = (100, 100, 100)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-EMPTY = None
+import pygame
+import sys
+import main
 
 
 class InitialScreen:
     def __init__(self):
         self.human_player = None
         self.computer_player = None
+        self.titles = {
+            'Which game mode do you want to chose?': ["Human vs Human", "Computer vs Human", "Computer vs Computer"],
+            'In which board size would you like to play with?': ["3 X 3", "5 X 5", "5 X 9"],
+            'What is your difficulty level?': ["Easy", "Medium", "Hard"],
+            'White always goes first. Do you want to be white or black?': ["White", "Black"],
+        }
+        self.selected_options = {}
 
-        # Initialize pygame and the main clock
-        pygame.init()
-        self.main_clock = pygame.time.Clock()
+    def draw_option(self, title, options, y):
+        # Choose game mode
+        text = main.BIG_FONT.render(title, True, main.BLACK)
+        rect = text.get_rect()
+        rect.center = (int(main.WINDOW_WIDTH * 0.5), int(main.WINDOW_HEIGHT * y))
 
-        # Set up the window
-        self.WINDOW_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('Fanorona')
-        self.SMALL_FONT = pygame.font.Font(None, 8)
-        self.BIG_FONT = pygame.font.Font(None, 12)
+        text_list = []
+        delta_x = 1./(len(options)+1)
+        x = delta_x
+        text_list.append((text, rect, title, False, False))
+        for opt in options:
+            surf = main.BIG_FONT.render(opt, True, main.BLACK)
+            rect = surf.get_rect()
+            rect.center = (int(main.WINDOW_WIDTH * x), int(main.WINDOW_HEIGHT * (y + 0.1)))
+            selected = self.selected_options.get(title, None) == opt
+            text_list.append((surf, rect, opt, selected, True))
+            x += delta_x
+
+        return text_list
+
+    def draw_all_options(self):
+        y = 0.1
+        delta_y = (1 / len(self.titles))
+        text_list = []
+        for key, value in self.titles.items():
+            text_list.append(self.draw_option(key, value, y))
+            y += delta_y
+
+        return text_list
 
     def draw_initial_screen(self):
         """Draws the text and handles the mouse click events for letting
@@ -34,123 +51,34 @@ class InitialScreen:
         Returns [WHITE_TOKEN, BLACK_TOKEN] if the player chooses to be White,
         [BLACK_TOKEN, WHITE_TOKEN] if Black.
         """
-        gamemode_choice = None
-        difficulty_choice = None
-        token_choice = None
-        board_choice = None
-
-        # Choose game mode
-        text_gamemode = self.BIG_FONT.render('Which game mode do you want to chose?', True, BLACK)
-        text_gamemode_rect = text_gamemode.get_rect()
-        text_gamemode_rect.center = (int(WINDOW_WIDTH * 0.5), int(WINDOW_HEIGHT * 0.1))
-
-        hh = self.BIG_FONT.render('Human vs Human', True, BLACK)
-        hh_rect = hh.get_rect()
-        hh_rect.center = (int(WINDOW_WIDTH * 0.175), int(WINDOW_HEIGHT * 0.2))
-
-        ch = self.BIG_FONT.render('Computer vs Human', True, BLACK)
-        ch_rect = ch.get_rect()
-        ch_rect.center = (int(WINDOW_WIDTH * 0.375), int(WINDOW_HEIGHT * 0.2))
-
-        cc = self.BIG_FONT.render('Computer vs Computer', True, BLACK)
-        cc_rect = cc.get_rect()
-        cc_rect.center = (int(WINDOW_WIDTH * 0.625), int(WINDOW_HEIGHT * 0.2))
-
-        # Choose board size
-        text_boardsize = self.BIG_FONT.render('In which board size would you like to play with?', True, BLACK)
-        text_boardsize_rect = text_boardsize.get_rect()
-        text_boardsize_rect.center = (int(WINDOW_WIDTH * 0.5), int(WINDOW_HEIGHT * 0.3))
-
-        x33 = self.BIG_FONT.render('3 X 3', True, BLACK)
-        x33_rect = x33.get_rect()
-        x33_rect.center = (int(WINDOW_WIDTH * 0.175), int(WINDOW_HEIGHT * 0.4))
-
-        x55 = self.BIG_FONT.render('5 x 5', True, BLACK)
-        x55_rect = x55.get_rect()
-        x55_rect.center = (int(WINDOW_WIDTH * 0.375), int(WINDOW_HEIGHT * 0.4))
-
-        x59 = self.BIG_FONT.render('5 x 9', True, BLACK)
-        x59_rect = x59.get_rect()
-        x59_rect.center = (int(WINDOW_WIDTH * 0.625), int(WINDOW_HEIGHT * 0.4))
-
-        # Choose difficulty
-        text_diff = self.BIG_FONT.render('What is your difficulty level?', True, BLACK)
-        text_diff_rect = text_diff.get_rect()
-        text_diff_rect.center = (int(WINDOW_WIDTH * 0.5), int(WINDOW_HEIGHT * 0.5))
-
-        easy = self.BIG_FONT.render('Easy', True, BLACK)
-        easy_rect = easy.get_rect()
-        easy_rect.center = (int(WINDOW_WIDTH * 0.175), int(WINDOW_HEIGHT * 0.6))
-
-        medium_surf = self.BIG_FONT.render('Medium', True, BLACK)
-        medium_rect = medium_surf.get_rect()
-        medium_rect.center = (int(WINDOW_WIDTH * 0.375), int(WINDOW_HEIGHT * 0.6))
-
-        hard_surf = self.BIG_FONT.render('Hard', True, BLACK)
-        hard_rect = hard_surf.get_rect()
-        hard_rect.center = (int(WINDOW_WIDTH * 0.625), int(WINDOW_HEIGHT * 0.6))
-
-        # Choose token color
-        text_surf = self.BIG_FONT.render('White always goes first. Do you want to be white or black?', True, BLACK)
-        text_surf_rect = text_surf.get_rect()
-        text_surf_rect.center = (int(WINDOW_WIDTH * 0.5), int(WINDOW_HEIGHT * 0.7))
-
-        white_surf = self.BIG_FONT.render('White', True, BLACK)
-        white_rect = white_surf.get_rect()
-        white_rect.center = (int(WINDOW_WIDTH * 0.375), int(WINDOW_HEIGHT * 0.8))
-
-        black_surf = self.BIG_FONT.render('Black', True, BLACK)
-        black_rect = black_surf.get_rect()
-        black_rect.center = (int(WINDOW_WIDTH * 0.625), int(WINDOW_HEIGHT * 0.8))
+        text_list = self.draw_all_options()
 
         while True:  # keep looping until the player has clicked on a color
-            self.main_clock.tick(FPS)  # CPU is too fast that slowing down is needed
+            main.main_clock.tick(main.FPS)  # CPU is too fast that slowing down is needed
+            main.WINDOW_SURF.fill(main.WHITE)
+            for opt in text_list:
+                for i, (surf, rect, text, selected, selectable) in enumerate(opt):
+                    color = main.GREEN if selected else main.BLACK # change text color based on selection status
+                    if selectable:
+                        new_surf = main.BIG_FONT.render(text, True, color)
+                        main.WINDOW_SURF.blit(new_surf, rect)
+                        opt[i] = (new_surf, rect, text, selected, selectable)  # update the text_list to reflect changes
+                    else:
+                        main.WINDOW_SURF.blit(surf, rect)
             for event in pygame.event.get():  # event handling loop
-                self.main_clock.tick(FPS)
-                if event.type == MOUSEBUTTONDOWN and event.button == 1:
+                main.main_clock.tick(main.FPS)
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == main.MOUSEBUTTONDOWN and event.button == 1:
                     # print 'click'
                     mouse_x, mouse_y = event.pos
-                    if white_rect.collidepoint((mouse_x, mouse_y)):
-                        white_surf = self.BIG_FONT.render('White', True, GREEN)
-                        token_choice = [WHITE, BLACK]
-                    elif black_rect.collidepoint((mouse_x, mouse_y)):
-                        black_surf = self.BIG_FONT.render('White', True, GREEN)
-                        token_choice = [BLACK, WHITE]
-                    elif hard_rect.collidepoint((mouse_x, mouse_y)):
-                        difficulty_choice = 'hard'
-                    elif medium_rect.collidepoint((mouse_x, mouse_y)):
-                        difficulty_choice = 'medium'
-                    elif easy_rect.collidepoint((mouse_x, mouse_y)):
-                        difficulty_choice = 'easy'
-                    elif x59_rect.collidepoint((mouse_x, mouse_y)):
-                        token_choice = 'x59'
-                    elif x55_rect.collidepoint((mouse_x, mouse_y)):
-                        token_choice = 'x55'
-                    elif x33_rect.collidepoint((mouse_x, mouse_y)):
-                        token_choice = 'x33'
-                    elif cc_rect.collidepoint((mouse_x, mouse_y)):
-                        token_choice = 'cc'
-                    elif ch_rect.collidepoint((mouse_x, mouse_y)):
-                        token_choice = 'ch'
-                    elif hh_rect.collidepoint((mouse_x, mouse_y)):
-                        token_choice = 'hh'
-
-            self.WINDOW_SURF.fill(WHITE)
-            self.WINDOW_SURF.blit(text_gamemode, text_gamemode_rect)
-            self.WINDOW_SURF.blit(hh, hh_rect)
-            self.WINDOW_SURF.blit(ch, ch_rect)
-            self.WINDOW_SURF.blit(cc, cc_rect)
-            self.WINDOW_SURF.blit(text_boardsize, text_boardsize_rect)
-            self.WINDOW_SURF.blit(x33, x33_rect)
-            self.WINDOW_SURF.blit(x55, x55_rect)
-            self.WINDOW_SURF.blit(x59, x59_rect)
-            self.WINDOW_SURF.blit(text_diff, text_diff_rect)
-            self.WINDOW_SURF.blit(easy, easy_rect)
-            self.WINDOW_SURF.blit(medium_surf, medium_rect)
-            self.WINDOW_SURF.blit(hard_surf, hard_rect)
-            self.WINDOW_SURF.blit(text_surf, text_surf_rect)
-            self.WINDOW_SURF.blit(white_surf, white_rect)
-            self.WINDOW_SURF.blit(black_surf, black_rect)
-            self.main_clock.tick(FPS)
+                    for opt in text_list:
+                        for i, (surf, rect, text, selected, selectable) in enumerate(opt):
+                            if rect.collidepoint((mouse_x, mouse_y)) and selectable:
+                                self.selected_options[opt[0][2]] = opt[i][2]  # update selected option
+                                for j, (s, r, t, sel, sela) in enumerate(opt):
+                                    opt[j] = (s, r, t, t == opt[i][2], True)  # update other options
+            main.main_clock.tick(main.FPS)
             pygame.display.update()
-            self.main_clock.tick(FPS)
+            main.main_clock.tick(main.FPS)
--- code/main.py
+++ code/main.py
@@ -19,5 +19,15 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 EMPTY = None
 
+# Initialize pygame and the main clock
+pygame.init()
+main_clock = pygame.time.Clock()
+
+# Set up the window
+WINDOW_SURF = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Fanorona')
+SMALL_FONT = pygame.font.Font(None, 8)
+BIG_FONT = pygame.font.Font(None, 12)
+
 screen = InitialScreen()
 screen.draw_initial_screen()
